cb88987c71ef62582e5b60d5380c917a437f7264
xneyk <aguadenadaledgar@gmail.com>
2024-04-28 01:26:31 +0200

    feat: especificación provisional de las clases Product i ProductSet

---
+++ Product.hh
@@ -0,0 +1,55 @@
+/**
+ * @file Product.hh
+ * @brief Especificación de la clase Product
+ */
+
+#ifndef _PRODUCT_HH_
+#define _PRODUCT_HH_
+
+class Product {
+private:
+
+   int weight;
+   int volume;
+
+public:
+
+   // Constructoras
+
+   /**
+    * @brief Creadora de productos.
+    * 
+    * Cualquier producto tiene un peso y un volumen concretos.
+    * \pre Cierto.
+    * \warning En ningún momento se ha aclarado que peso y volumen deban ser > 0.
+    * \post Se ha creado una instancia de producto con peso = "weight" i volumen = "volume".
+   */
+   Product(int weight, int volume); // el identificador es natural en un conjunto de productos.
+
+   // Consultoras
+
+   /**
+    * @brief Consultora del peso de un producto
+    * \pre Cierto.
+    * \post Retorna el peso del parámetro implícito
+   */
+   int getWeight() const;
+
+   /**
+    * @brief Consultora del volumen de un producto
+    * \pre Cierto.
+    * \post Retorna el volumen del parámetro implícito
+   */
+   int getVolume() const;
+
+   // Output / Escritura
+
+   /**
+    * @brief Método de output de un producto.
+    * \pre Cierto.
+    * \post Se ha escrito por el canal estándar el peso y volumen del producto (en dicho orden siguiendo el formato: "peso volumen")
+   */
+   void print() const;
+};
+
+#endif
---
+++ ProductSet.hh
@@ -0,0 +1,69 @@
+/**
+ * @file ProductSet.hh
+ * @brief Especificación de la classe ProductSet
+ * 
+ * Dicha clase, representa un conjunto de productos (clase Product). Cada producto se identifica con un id numérico.
+*/
+#ifndef _PRODUCT_SET_HH_
+#define _PRODUCT_SET_HH_
+
+#ifndef NO_DIAGRAM
+#include <map>
+#endif
+
+#include "Product.hh"
+using namespace std;
+
+class ProductSet {
+private:
+
+   // Se ha decidido representar el conjunto de productos como 
+   // un mapa de nombre del producto --> características del producto
+   map<int,Product> setOfProducts;
+
+public:
+
+   // Constructoras
+
+   /**
+    * @brief Creadora por defecto
+    * \pre Cierto.
+    * \post Crea un nuevo conjunto de producto vacío (sin producto)
+   */
+   ProductSet();
+
+   // Modificadoras
+
+   /**
+    * @brief Añade un producto al conjunto.
+    * \pre Cierto.
+    * \warning En ningún momento se ha aclarado que peso y volumen deban ser > 0.
+    * \post se ha añadido un nuevo producto al parámetro implícito, con su peso y volumen correspondiente, al conjunto de productos. el identificador de éste es el identificador del anterior + 1 en caso de que el conjunto no esté vacío, 1 en otro caso.
+   */
+   void addProduct(Product product);
+
+   // Consultoras
+
+   /**
+    * @brief Consultora del nombre de productos diferentes en el conjunto.
+    * \pre Cierto.
+    * \post Retorna el múmero de productos diferents en el parámetro implícito.
+   */
+   int getTotalProducts() const;
+
+   /**
+    * @brief Consultora del peso de un producto perteneciente al conjunto
+    * \pre el producto con identificador "id" pertenece al conjunto.
+    * \post Retorna el peso del producto con con identificador "id" contenido en el parámetro ímplicito
+   */
+   int getWeightById(int id) const;
+
+   /**
+    * @brief Consultora del volument de un producto perteneciente al conjunto
+    * \pre el producto con identificador "id" pertenece al conjunto.
+    * \post Retorna el volument del producto con con identificador "id" contenido en el parámetro ímplicito
+   */
+   int getVolumebyId(int id) const;
+};
+
+#endif
