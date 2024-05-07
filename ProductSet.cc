2ed9bab2de3a64596716d2cf3063119ab9305e28
xneyk <aguadenadaledgar@gmail.com>
2024-05-07 17:06:55 +0200

    feat: implementación de la clase ProductSet (listo para merge)

--- ProductSet.cc
+++ ProductSet.cc
@@ -13,3 +13,25 @@ void ProductSet::addProduct(Product product) {
    setOfProducts[lastIdAdded + 1] = product;
    ++lastIdAdded;
 }
+
+// Consultoras
+
+bool ProductSet::existsProductWithId(int id) const {
+   return 1 <= id <= lastIdAdded;
+}
+
+int ProductSet::getTotalProducts() const {
+   return lastIdAdded;
+}
+
+int ProductSet::getWeightById(int id) const {
+   // map::at(T key) throw error si el elemento con clave "key" no existe.
+   // --> nunca va a pasar los errores se controlan en main.
+   return setOfProducts.at(id).getWeight();
+}
+
+int ProductSet::getVolumeById(int id) const {
+   // map::at(T key) throw error si el elemento con clave "key" no existe.
+   // --> nunca va a pasar los errores se controlan en main.
+   return setOfProducts.at(id).getVolume();
+}
