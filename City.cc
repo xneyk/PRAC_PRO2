13ccd91fdb5ab3bb8f468947bf78375ea7b14741
xneyk <aguadenadaledgar@gmail.com>
2024-05-09 18:47:12 +0200

    feat: se elimina la clase itermedia City y se cambia el concepto inventario a ciudad

--- City.cc
+++ City.cc
@@ -1,8 +1,8 @@
-#include "Inventory.hh"
+#include "City.hh"
 
 // Constructoras
 
-Inventory::Inventory() {
+City::City() {
    inventory = map<int,ProductInventoryStats>();
    totalWeight = 0;
    totalVolume = 0;
@@ -10,13 +10,13 @@ Inventory::Inventory() {
 
 // Modificadoras
 
-void Inventory::addProduct(int id, const ProductSet &product_set, int owned, int needed) {
+void City::addProduct(int id, const ProductSet &product_set, int owned, int needed) {
    inventory[id] = ProductInventoryStats(owned, needed);
    totalWeight += owned*product_set.getWeightById(id);
    totalVolume += owned*product_set.getVolumeById(id);
 }
 
-void Inventory::setProductStatus(int id, const ProductSet &product_set, int new_owned, int new_needed) {
+void City::setProductStatus(int id, const ProductSet &product_set, int new_owned, int new_needed) {
    int old_owned = getOwnedById(id);
    inventory[id].setOwned(new_owned);
    inventory[id].setNeeded(new_needed);
@@ -25,7 +25,7 @@ void Inventory::setProductStatus(int id, const ProductSet &product_set, int new_
    totalVolume += (new_owned - old_owned)*product_set.getVolumeById(id);
 }
 
-void Inventory::removeProduct(int id, const ProductSet &product_set) {
+void City::removeProduct(int id, const ProductSet &product_set) {
    int old_owned = getOwnedById(id);
    inventory.erase(id);
    totalWeight -= old_owned*product_set.getWeightById(id);
@@ -34,19 +34,19 @@ void Inventory::removeProduct(int id, const ProductSet &product_set) {
 
 // Consultoras
 
-int Inventory::getTotalWeight() {
+int City::getTotalWeight() {
    return totalWeight;
 }
 
-int Inventory::getTotalVolume() {
+int City::getTotalVolume() {
    return totalVolume;
 }
 
-int Inventory::getOwnedById(int id) const {
+int City::getOwnedById(int id) const {
    return inventory.at(id).getOwned();
 }
 
-int Inventory::getNeededById(int id) const {
+int City::getNeededById(int id) const {
    return inventory.at(id).getNeeded();
 }
 
@@ -54,8 +54,8 @@ int Inventory::getNeededById(int id) const {
 
 // IO (Lectura - Escritura)
 
-void Inventory::read(const ProductSet &product_set) {
-   (*this) = Inventory(); // Entiendo que si volvemos a leer el inventario se debe sobre escribir.
+void City::read(const ProductSet &product_set) {
+   (*this) = City(); // Entiendo que si volvemos a leer el inventario se debe sobre escribir.
    int number_of_products;
    cin >> number_of_products;
    int id, owned, needed;
@@ -65,7 +65,7 @@ void Inventory::read(const ProductSet &product_set) {
    }
 }
 
-void Inventory::print() const {
+void City::print() const {
    map<int,ProductInventoryStats>::const_iterator inventory_line = inventory.begin();
    while (inventory_line != inventory.end()) {
       cout << inventory_line->first << ' ';
--- test_city.cc
+++ test_city.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-#include "Inventory.hh"
+#include "City.hh"
 #include "ProductSet.hh"
 
 int main() {
    ProductSet product_set;
-   Inventory inventory;
+   City city;
 
    string cmd;
    int id, need, own;
@@ -40,43 +40,43 @@ int main() {
       else if (cmd == "addProduct") {
          cin >> id >> own >> need;
          cout << "#addProduct{" << id << "}<--[" << own << "," << need << "]" << endl;
-         inventory.addProduct(id, product_set, own, need);
+         city.addProduct(id, product_set, own, need);
       }
       else if(cmd == "setProductStatus") {
          cin >> id >> own >> need;
          cout << "#setProductStatus{" << id << "}<--[" << own << "," << need << "]" << endl;
-         inventory.setProductStatus(id, product_set, own, need);
+         city.setProductStatus(id, product_set, own, need);
       }
       else if(cmd == "removeProduct") {
          cin >> id;
          cout << "#removeProduct{" << id << "}" << endl;
-         inventory.removeProduct(id, product_set);
+         city.removeProduct(id, product_set);
       }
       else if(cmd == "getTotalWeight") {
          cout << "#getTotalWeight" << endl;
-         cout << inventory.getTotalWeight() << endl;
+         cout << city.getTotalWeight() << endl;
       }
       else if(cmd == "getTotalVolume") {
          cout << "#getTotalVolume" << endl;
-         cout << inventory.getTotalVolume() << endl;
+         cout << city.getTotalVolume() << endl;
       }
       else if(cmd == "getOwnedById") {
          cin >> id;
          cout << "#getOwnedById{" << id << "}" << endl;
-         cout << inventory.getOwnedById(id) << endl;
+         cout << city.getOwnedById(id) << endl;
       }
       else if(cmd == "getNeededById") {
          cin >> id;
          cout << "#getNeededById{" << id << "}" << endl;
-         cout << inventory.getNeededById(id) << endl;
+         cout << city.getNeededById(id) << endl;
       }
       else if(cmd == "read") {
          cout << "#read" << endl;
-         inventory.read(product_set);
+         city.read(product_set);
       }
       else if(cmd == "print") {
          cout << "#print" << endl;
-         inventory.print();
+         city.print();
       }
    }
 }
